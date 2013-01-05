61418509a4d4a7abc4df1d61f13f9679145f422a
Kitton <Gaf@Gaf350U>
2013-01-05 23:51:08 +0100

    + Add 2D coordinates to Location.

--- locations/Location.java
+++ locations/Location.java
@@ -9,7 +9,17 @@ package locations;
  * @author gaspercat
  */
 public abstract class Location {
-    public Location(){
-        
+	//Simple 2D euclidean coordinates.
+	protected int x,y;
+	
+    public Location(int x, int y){
+        this.x = x;
+        this.y = y;
     }
+    
+    public int getX() { return x;}
+    public int getY() { return y;}
+    
+    public void setX(int x) { this.x = x;}
+    public void setY(int y) { this.y = y;}
 }
